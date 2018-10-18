e4acda3f78dfa95cff2c11fe491012b7939a3176
Thừa Nguyễn <ngocthua92@live.com>
2018-10-18 22:20:47 +0700

    First commit

---
+++ app/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use GetCandy\Api\Traits\HasCandy;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+    use HasCandy;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
